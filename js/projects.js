b2424b2601f2327259422b0a8e3989fa32423c37
Ricardo Golac <rigoba98@hotmail.com>
2021-01-04 13:16:29 -0500

    Update 2021

---
+++ js/projects.js
@@ -0,0 +1,19 @@
+var projects = [
+  {
+    name: "Polinex",
+    description: "A hub for political news and demographics.",
+    technologies: ["NodeJS", "React", "Heroku", "MongoDB", "Agile"],
+    links: [
+      {
+        type: "Website",
+        link: "https://damp-beyond-30089.herokuapp.com/"
+      },
+      {
+        type: "Github",
+        link: "https://github.com/RicardoGolac/PolinexWebsite"
+      }
+    ],
+    image: "images/polinex.png",
+    tags: ["School"]
+  }
+];
